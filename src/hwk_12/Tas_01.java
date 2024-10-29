b39b9ce954ded054078acd17dab17d678f604c5d
Wladimir <jakowlewitch@yahoo.de>
2024-10-29 20:35:44 +0100

    lesson

---
+++ src/hwk_12/Tas_01.java
@@ -0,0 +1,23 @@
+package hwk_12;
+
+
+/**
+ * Author Waldemar Ilz
+ *{code data} 22.10.2024
+ */
+
+public class Tas_01 {
+
+    /*
+
+Важно!!!
+https://git-scm.com/downloads
+
+На сайте git выбираем и устанавливаем git для вашей операционной системы\
+https://github.com/
+
+Регистрируемся на GitHub. Запоминаем (или лучше записываем) name, email и пароль, который вы используете для регистрации
+
+
+     */
+}
---
+++ src/lesson_14/classArrays.java
@@ -0,0 +1,11 @@
+package lesson_14;
+
+/**
+ * Author Waldemar Ilz
+ *{code data} 29.10.2024
+ */
+
+public class classArrays {
+
+
+}
