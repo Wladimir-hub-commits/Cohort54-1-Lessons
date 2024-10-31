e66ebbfee82b35e4ebdec9343d7888f6c26d6d65
Wladimir <jakowlewitch@yahoo.de>
2024-10-31 17:09:39 +0100

    hwk_14 add

---
+++ src/hwk_14/Task_1.java
@@ -0,0 +1,34 @@
+package hwk_14;
+
+/**
+ * Author Waldemar Ilz
+ *{code data} 31.10.2024
+ */
+
+/*
+Task 1
+Сумма четных чисел
+Напишите метод, который вычисляет сумму всех четных чисел в массиве.
+ */
+
+public class Task_1 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int result = sumEvenNumbers(array);
+        System.out.println("Сумма четных чисел: " + result);
+
+    }//Methods area
+
+    public static int sumEvenNumbers(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                sum += number;
+            }
+        }
+        return sum;
+    }
+
+
+} // End class
