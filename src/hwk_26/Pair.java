aaf1603d171d804863a1eae27831df839c196162
Wladimir <jakowlewitch@yahoo.de>
2024-12-12 18:04:48 +0100

    hwk_26 add

--- src/hwk_26/Pair.java
+++ src/hwk_26/Pair.java
@@ -9,30 +9,45 @@ public class Pair <T>{
     private T first;
     private T second;
 
+    // Конструктор, принимающий два параметра типа T
     public Pair(T first, T second) {
         this.first = first;
         this.second = second;
     }
 
+    // Метод для получения первого элемента
     public T getFirst() {
         return first;
     }
 
+    // Метод для получения второго элемента
     public T getSecond() {
         return second;
     }
 
+    // Метод для установки значения первого элемента
     public void setFirst(T value) {
         this.first = value;
     }
 
+    // Метод для установки значения второго элемента
     public void setSecond(T value) {
         this.second = value;
     }
+
+    // Метод для обмена местами первого и второго элементов
     public void swap(){
         T temp = first;
         first  = second;
         second = temp;
 
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
     }
 }
